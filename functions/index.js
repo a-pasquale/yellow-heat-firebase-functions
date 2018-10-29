cddd402803a62e74f0d147d23695820ff3526c3e
Andrew Pasquale <andrew@elytra.net>
2018-10-29 10:27:53 -0400

    Use new data structure and update temp.

--- functions/index.js
+++ functions/index.js
@@ -24,59 +24,69 @@ const mailTransport = nodemailer.createTransport({
   },
 });
 
+// Update current temperature 
+exports.currentTemp = functions.database.ref('/{uid}/{heater}/temp/{id}')
+    .onWrite(
+        (data, context) => {
+            const temp = round(data.after.val().temp, 1);
+            console.log("Temp: ", temp);
+            return admin.database().ref('/users/'+context.params.uid+'/'+context.params.heater).once('value').then(
+                (snap) => {
+                    return snap.ref.child('temp').set(temp);
+                }
+            )
+        }
+    )
+
 // Update fuel usage for Yellow Heat database
 exports.calcFuelUse = functions.database.ref('/{uid}/{heater}/data/{id}')
     .onWrite((data, context) => {
 
-        return data.after.ref.parent.parent.once('value').then(
-            (heater) => {
+        const fuelReading = round(data.after.val().fuel, 4);
+        console.log("Fuel Reading: ", fuelReading);
 
-                const fuelReading = round(data.after.val().fuel, 4);
-                console.log("Fuel Reading: ", fuelReading);
-
-                // Low Fuel Notifications
-                if (fuelReading < 0.5) {
-                    admin.database().ref('/users/'+context.params.uid).once('value').then(
-                        (snap) => {
-                            const user = snap.val();
-                            const mailOptions = {
-                                from: `Holyoke Codes <holyokecodes@gmail.com>`,
-                                to: user.email,
-                            };
-                        
-                            // The user subscribed to the newsletter.
-                            mailOptions.subject = `Alert: Fuel level low!`;
-                            mailOptions.text = `Hey ${user.name || ''}! Your fuel level is at ${fuelReading}.`;
-                            return mailTransport.sendMail(mailOptions).then(() => {
-                                return console.log('Alert email sent to:', user.email);
-                            });
-                        }
-                    )
-                }
-
-                const tankSize = heater.child('tankSize').val();
-                if (!heater.child('lastFuelReading')) {
-                    heater.child('lastFuelReading').set('')
-                }
-                if (!heater.child('totalFuelUse')) {
-                    heater.child('totalFuelUse').set('')
-                    console.log("Setting totalFuelUse")
-                }
-                const lastFuelReading = heater.child('lastFuelReading').val();
-                const fuelUse = round((lastFuelReading - fuelReading) * tankSize, 2);
-                console.log("fuelUse: ", fuelUse)
-                let totalFuelUse = heater.child('totalFuelUse').val();
-                console.log("totalfuelUse: ", totalFuelUse)
+        // Low Fuel Notifications
+        if (fuelReading < 0.5) {
+            admin.database().ref('/users/'+context.params.uid).once('value').then(
+                (snap) => {
+                    const user = snap.val();
+                    const mailOptions = {
+                        from: `Holyoke Codes <holyokecodes@gmail.com>`,
+                        to: user.email,
+                    };
                 
+                    // Send an alert notification.
+                    mailOptions.subject = `Alert: Fuel level low!`;
+                    mailOptions.text = `Hey ${user.name || ''}! Your fuel level is at ${fuelReading}.`;
+                    return mailTransport.sendMail(mailOptions).then(() => {
+                        return console.log('Alert email sent to:', user.email);
+                    });
+                }
+            )
+        }
+        // Update heater summary statistics
+        return admin.database().ref('/users/'+context.params.uid+'/'+context.params.heater).once('value').then(
+            (snap) => {
+                // Calculate how much fuel used since last reading
+                const lastFuelReading = snap.val().lastFuelReading;
+                const tankSize = snap.val().tankSize;
+                const fuelUse = round((lastFuelReading - fuelReading) * tankSize, 2);
+                console.log("fuel use: ", fuelUse)
+
+                // Update total fuel use
+                let totalFuelUse = snap.val().totalFuelUse;
+                console.log("Previous total fuel use: ", totalFuelUse)
                 if (fuelUse > 0) {
-                    totalFuelUse += fuelUse;
+                    totalFuelUse += round(fuelUse, 1);
                     console.log('User %s used %s gallons of fuel with heater %s', context.params.uid, fuelUse, context.params.heater);
                 } else {
                     console.log('User %s added %s gallons of fuel to heater %s', context.params.uid, -fuelUse, context.params.heater);
                 }
 
-                data.after.ref.parent.parent.child('lastFuelReading').set(fuelReading);
-                return data.after.ref.parent.parent.child('totalFuelUse').set(totalFuelUse);
+                // Update summary stats
+                snap.ref.child('lastFuelReading').set(fuelReading);
+                snap.ref.child('status').set(data.after.val().message);
+                return snap.ref.child('totalFuelUse').set(totalFuelUse);
             }
         )        
     })
